3f92bcd4f9612f107af9676a2198336a2bad6f59
devRena <renatofelipeherrera.c@gmail.com>
2026-06-23 05:25:14 -0400

    Minor changes

--- routes/mapRoutes.js
+++ routes/mapRoutes.js
@@ -2,8 +2,6 @@ import { Router } from "express";
 
 export const mapRoutes = Router();
 
-mapRoutes.get("/", (req, res) => {
-  res.json({ message: "Hello World" });
-});
+mapRoutes.get("/", (req, res) => {});
 mapRoutes.get("/:id", (req, res) => {});
 mapRoutes.post("/", (req, res) => {});
