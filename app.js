745bdfa6498eb56e60390600437142bd5d202576
devRena <renatofelipeherrera.c@gmail.com>
2026-06-24 03:31:42 -0400

    The routes were created using Express Router.

--- app.js
+++ app.js
@@ -1,35 +1,11 @@
 import express from "express";
 import logger from "morgan";
-import mongoConnection from "./server/server.js";
-import mapRoutes from "./schemas/mapRoutes.js";
-import { routeValidate } from "./schemas/rutaScheme.js";
+import { mapRoutesRouter } from "./routes/mapRoutes.routes.js";
 
 const app = express();
 app.use(express.json());
 app.use(logger("dev"));
 
-app.get("/api/routes", async (req, res) => {
-  const data = await mapRoutes.find();
-
-  return res.status(200).json(data);
-});
-
-app.get("/api/routes/:id", async (req, res) => {
-  const { id } = req.params;
-
-  const data = await mapRoutes.findById(id);
-
-  if (!data) return res.status(404).json({ message: "Route not found" });
-});
-
-app.post("/api/routes/", async (req, res) => {
-  const { error, data, success } = routeValidate(req.body);
-
-  if (!success) return res.json({ message: JSON.parse(error.message) });
-
-  const created = await mapRoutes.create(data);
-
-  return res.status(200).json(created);
-});
+app.use("/api/routes", mapRoutesRouter);
 
 export default app;
